a73a1e6ccdaabcd27c6fb00dd13a9627240c7e62
MOREY MATERISE <materise@us.ibm.com>
2021-04-05 14:45:01 -0400

    xgoal progress

--- models/xgoal.py
+++ models/xgoal.py
@@ -1,9 +1,12 @@
 from sklearn.ensemble import GradientBoostingRegressor, GradientBoostingClassifier
 from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import RepeatedKFold, RepeatedStratifiedKFold, GridSearchCV
+from sklearn.model_selection import RepeatedKFold, RepeatedStratifiedKFold, GridSearchCV, train_test_split
+from sklearn.metrics import mean_squared_error
 from numpy import mean, std, arange
 import pandas as pd
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+import pickle
+
 
 def setup_data():
     df = pd.read_csv('../NHLData/moneypuck/shots/shots_2019.csv')
@@ -14,36 +17,7 @@ def setup_data():
     df = df[df.shotWasOnGoal == 1]
 
     columns_to_use = ['time', 'goal', 'shotAngleAdjusted', 'shotDistance', 'shotType', 'offWing']
-    columns_to_drop = ['shotID', 'arenaAdjustedShotDistance', 'arenaAdjustedXCord', 'arenaAdjustedXCordABS', 'arenaAdjustedYCord', 'arenaAdjustedYCordAbs', 'averageRestDifference',
-    'awayEmptyNet', 'awayPenalty1Length', 'awayPenalty1TimeLeft', 'awaySkatersOnIce', 'awayTeamCode', 'awayTeamGoals', 'defendingTeamAverageTimeOnIce', 'defendingTeamAverageTimeOnIceOfDefencemen',
-    'defendingTeamAverageTimeOnIceOfDefencemenSinceFaceoff', 'defendingTeamAverageTimeOnIceOfForwards', 'defendingTeamAverageTimeOnIceOfForwardsSinceFaceoff',
-    'defendingTeamAverageTimeOnIceSinceFaceoff', 'defendingTeamDefencemenOnIce', 'defendingTeamForwardsOnIce', 'defendingTeamMaxTimeOnIce', 'defendingTeamMaxTimeOnIceOfDefencemen',
-    'defendingTeamMaxTimeOnIceOfDefencemenSinceFaceoff', 'defendingTeamMaxTimeOnIceOfForwards',	'defendingTeamMaxTimeOnIceOfForwardsSinceFaceoff', 'defendingTeamMaxTimeOnIceSinceFaceoff',
-    'defendingTeamMinTimeOnIce', 'defendingTeamMinTimeOnIceOfDefencemen', 'defendingTeamMinTimeOnIceOfDefencemenSinceFaceoff', 'defendingTeamMinTimeOnIceOfForwards',
-    'defendingTeamMinTimeOnIceOfForwardsSinceFaceoff', 'defendingTeamMinTimeOnIceSinceFaceoff',	'distanceFromLastEvent', 'event', 'game_id',
-    #'goal',
-    'goalieIdForShot', 'goalieNameForShot',
-    'homeEmptyNet', 'homePenalty1Length', 'homePenalty1TimeLeft', 'homeSkatersOnIce', 'homeTeamCode', 'homeTeamGoals', 'homeTeamWon', 'id', 'isHomeTeam', 'isPlayoffGame',
-    'lastEventCategory', 'lastEventShotAngle', 'lastEventShotDistance', 'lastEventTeam', 'lastEventxCord', 'lastEventxCord_adjusted', 'lastEventyCord', 'lastEventyCord_adjusted', 'location',
-    #'offWing',
-    'period', 'playerNumThatDidEvent', 'playerNumThatDidLastEvent', 'playerPositionThatDidEvent', 'season', 'shooterLeftRight', 'shooterName', 'shooterPlayerId',
-    'shooterTimeOnIce', 'shooterTimeOnIceSinceFaceoff', 'shootingTeamAverageTimeOnIce', 'shootingTeamAverageTimeOnIceOfDefencemen', 'shootingTeamAverageTimeOnIceOfDefencemenSinceFaceoff',
-    'shootingTeamAverageTimeOnIceOfForwards', 'shootingTeamAverageTimeOnIceOfForwardsSinceFaceoff', 'shootingTeamAverageTimeOnIceSinceFaceoff', 'shootingTeamDefencemenOnIce',
-    'shootingTeamForwardsOnIce', 'shootingTeamMaxTimeOnIce', 'shootingTeamMaxTimeOnIceOfDefencemen', 'shootingTeamMaxTimeOnIceOfDefencemenSinceFaceoff', 'shootingTeamMaxTimeOnIceOfForwards',
-    'shootingTeamMaxTimeOnIceOfForwardsSinceFaceoff', 'shootingTeamMaxTimeOnIceSinceFaceoff', 'shootingTeamMinTimeOnIce', 'shootingTeamMinTimeOnIceOfDefencemen',
-    'shootingTeamMinTimeOnIceOfDefencemenSinceFaceoff', 'shootingTeamMinTimeOnIceOfForwards', 'shootingTeamMinTimeOnIceOfForwardsSinceFaceoff', 'shootingTeamMinTimeOnIceSinceFaceoff', 'shotAngle',
-    #'shotAngleAdjusted',
-    'shotAnglePlusRebound', 'shotAnglePlusReboundSpeed', 'shotAngleReboundRoyalRoad',
-    #'shotDistance',
-    'shotGeneratedRebound', 'shotGoalieFroze', 'shotOnEmptyNet', 'shotPlayContinuedInZone', 'shotPlayContinuedOutsideZone', 'shotPlayStopped', 'shotRebound', 'shotRush',
-    #'shotType',
-    'shotWasOnGoal', 'speedFromLastEvent', 'team', 'teamCode',
-    #'time',
-    'timeDifferenceSinceChange', 'timeSinceFaceoff', 'timeSinceLastEvent', 'timeUntilNextEvent', 'xCord', 'xCordAdjusted', 'xFroze', 'xGoal', 'xPlayContinuedInZone',
-    'xPlayContinuedOutsideZone', 'xPlayStopped', 'xRebound', 'xShotWasOnGoal', 'yCord', 'yCordAdjusted']
-
-
-    df = df.drop(columns_to_drop, axis=1)
+    df = df[columns_to_use]
     df.reset_index(drop=True, inplace=True)
 
     output = df.goal
@@ -56,7 +30,6 @@ def setup_data():
     input['time'] = input['time'].map(lambda t: t/max_time)
     input['shotAngleAdjusted'] = input['shotAngleAdjusted'].map(lambda a: a/max_angle)
     input['shotDistance'] = input['shotDistance'].map(lambda sd: sd/max_distance)
-
     return input, output
 
 
@@ -97,19 +70,30 @@ def get_models_tree_depth():
         models[str(i)] = GradientBoostingClassifier(max_depth=i)
     return models
 
-
+# Best: -0.085554 using {'learning_rate': 0.01, 'max_depth': 4, 'n_estimators': 100, 'subsample': 0.3}
+## n_splits=3 test_size=0.95 n_repeats=3
 def grid_search():
-    input, output = setup_data()
-    model = GradientBoostingRegressor()
+    features, labels = setup_data()
+    print(str(features))
+    print(str(labels))
+    xtrain, xtest, ytrain, ytest=train_test_split(features, labels, random_state=12, test_size=0.95)
+    print(str(xtrain))
+    model = GradientBoostingClassifier()
     grid = dict()
     grid['n_estimators'] = [10, 50, 100, 500]
     grid['learning_rate'] = [0.0001, 0.001, 0.01, 0.1, 1.0]
     grid['subsample'] = [0.3, 0.5, 0.7, 1.0]
     grid['max_depth'] = [2, 3, 4, 7, 9]
-    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+    cv = RepeatedStratifiedKFold(n_splits=3, n_repeats=3, random_state=1)
+    print('folded')
     grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=cv, scoring='neg_mean_absolute_error')
-    grid_result = grid_search.fit(input, output)
+    print('grid search cv created')
+    grid_result = grid_search.fit(xtrain, ytrain)
     print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    f = open("gridsearch.txt", "w")
+    f.write("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    f.close()
     means = grid_result.cv_results_['mean_test_score']
     stds = grid_result.cv_results_['std_test_score']
     params = grid_result.cv_results_['params']
@@ -124,24 +108,66 @@ def evaluate_model(model, input, output):
     return scores
 
 
-def xgoal():
+def eval_all_models():
     input, output = setup_data()
     print(str(input))
-    models = get_models_tree_depth()
+    models = get_models_learning_rate()
     results, names = list(), list()
     for name, model in models.items():
         scores = evaluate_model(model, input, output)
         results.append(scores)
         names.append(name)
-        print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
+        print('>%s %.7f (%.7f)' % (name, mean(scores), std(scores)))
 
     pyplot.boxplot(results, labels=names, showmeans=True)
     pyplot.show()
 
 
-if __name__ == '__main__':
-    xgoal()
-    #grid_search()
+def xgoal_model():
+    features, labels = setup_data()
+    xtrain, xtest, ytrain, ytest=train_test_split(features, labels, random_state=12, test_size=0.25)
+    # with new parameters
+    gbc = GradientBoostingClassifier(n_estimators=100,
+        max_depth=4,
+        learning_rate=0.01,
+        subsample=0.3)
+
+    gbc.fit(xtrain, ytrain)
+    save_model(gbc, 'xgoal_gbc.sav')
+
+    ypred = gbc.predict(xtest)
+    mse = mean_squared_error(ytest, ypred)
+    print("MSE: %.5f" % mse)
+
+    ## for sorted graph
+    Z = [x for _,x in sorted(zip(ytest,ypred))]
+    V = ytest.tolist()
+    V.sort()
+    x_ax = range(len(V))
+    plt.scatter(x_ax, V, s=5, color="blue", label="original")
+    plt.plot(x_ax, Z, lw=0.8, color="red", label="predicted")
+
+    # x_ax = range(len(ytest))
+    # plt.scatter(x_ax, ytest, s=5, color="blue", label="original")
+    # plt.plot(x_ax, ypred, lw=0.8, color="red", label="predicted")
+
+    plt.legend()
+    plt.show()
+
+
+def save_model(model, filename):
+    # save the model to disk
+    pickle.dump(model, open(filename, 'wb'))
+
+
+def load_model(filename):
+    # load the model from disk
+    return pickle.load(open(filename, 'rb'))
+
+
+xgoal_model()
+#grid_search()
+
 
 
 
